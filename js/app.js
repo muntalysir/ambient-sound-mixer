d89c35e13e8c2bb2a842e6bb0a0cb463e0adc95b
Aly Albahri <muntasiralbahri@gmail.com>
2026-03-25 14:48:41 +0400

    UI Class & Sound Cards

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 import { sounds, defaultPresets } from './soundData.js';
 import { SoundManager } from './soundManager.js';
+import { UI } from './ui.js';
 
 class AmbientMixer {
   // initialize dependencies and default state
   constructor() {
     this.soundManager = new SoundManager();
-    this.ui = null;
+    this.ui = new UI();
     this.presetManager = null;
     this.timer = null;
     this.currentSoundState = {};
@@ -14,6 +15,12 @@ class AmbientMixer {
 
   init() {
     try {
+      // Initialize UI
+      this.ui.init();
+
+      // Render sound card using our sound data
+      this.ui.renderSoundCards(sounds);
+
       // this.soundManager.loadSound('rain', 'audio/rain.mp3');
       // Load all sounds
       this.loadAllSounds();
